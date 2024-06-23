f64473173b1bbbbfea6ec03b939fe6468ed9faac
redhottensors <159090586+redhottensors@users.noreply.github.com>
2024-06-23 16:15:12 -0600

    fix progress bars, fixed-step solvers, and add support for all monotonic schedules

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is an ALPHA release using [torchdiffeq](https://github.com/rtqichen/torchdi
 
 When time permits, a custom batched RK solver will be written and fixed-step methods will be deprecated.
 
-This node only supports ODE flow models like Stable Diffusion 3.
+This node supports all models, but is intended for use with ODE flow models like Stable Diffusion 3.
 
 # Installation
 Clone this repo into ``ComfyUI/custom_nodes`` or use [ComfyUI-Manager](https://github.com/ltdrdata/ComfyUI-Manager).
@@ -16,9 +16,10 @@ In your ComfyUI virtual environment, run ``pip install torchdiffeq``.
 
 The ODE Solver node is available at <ins>sampling > custom_sampling > samplers > ODE Solver</ins>.
 
-The choice of scheduler will generally make no difference, since the adaptive solvers only take a start point and an end point, and those should always be 1 and 0 respectively for SD3.  To keep things simple, just use sgm_uniform.
+The choice of scheduler will generally make no difference, since the adaptive solvers only take a start point and an end point, and those should always be 1 and 0 respectively for SD3. To keep things simple, just use ``sgm_uniform``.
 
-Similar to DPM Adaptive, these solvers will choose how many steps to use and how far those steps will go on their own, based on the tolerances set (more information below).  max_steps will not control how many steps the solvers take on their own, and it instead is provided as a failsafe so you don't end up having to wait for a very long solve if you accidentally set tolerances too low.
+Similar to DPM Adaptive, these solvers will choose how many steps to use and how far those steps will go on their own, based on the tolerances set (more information below).
+``max_steps`` will not control how many steps the solvers take on their own, and it instead is provided as a failsafe so you don't have to wait for a very long solve if your tolerances are too low.
 
 Here's some suggested starting points to try out the adaptive solvers.
 
--- nodes/nodes_ode.py
+++ nodes/nodes_ode.py
@@ -3,51 +3,81 @@ import torch
 import torchdiffeq
 from tqdm.auto import trange, tqdm
 
-ADAPTIVE_SOLVERS = { "euler", "midpoint", "rk4", "heun3", "explicit_adams", "implicit_adams" }
-FIXED_SOLVERS = { "dopri8", "dopri5", "bosh3", "fehlberg2", "adaptive_heun" }
+ADAPTIVE_SOLVERS = { "dopri8", "dopri5", "bosh3", "fehlberg2", "adaptive_heun" }
+FIXED_SOLVERS = { "euler", "midpoint", "rk4", "heun3", "explicit_adams", "implicit_adams" }
 SOLVERS = [ *ADAPTIVE_SOLVERS, *FIXED_SOLVERS ]
 SOLVERS.sort()
 
 class ODEFunction:
-    def __init__(self, model, n_steps, is_adaptive=False, extra_args=None, callback=None):
+    def __init__(self, model, t_min, t_max, n_steps, is_adaptive, extra_args=None, callback=None):
         self.model = model
         self.extra_args = {} if extra_args is None else extra_args
         self.callback = callback
+        self.t_min = t_min.item()
+        self.t_max = t_max.item()
         self.n_steps = n_steps
         self.is_adaptive = is_adaptive
-        self.pbar = tqdm(total=1.0 if is_adaptive else n_steps, leave=False, position=1)
+        self.step = 0
+
+        if is_adaptive:
+            self.pbar = tqdm(
+                total=100,
+                desc="solve",
+                unit="%",
+                leave=False,
+                position=1
+            )
+        else:
+            self.pbar = tqdm(
+                total=n_steps,
+                desc="solve",
+                leave=False,
+                position=1
+            )
 
     def __call__(self, t, y):
-        if t > (1.0 - 1e-5):
+        if t <= 1e-5:
             return torch.zeros_like(y)
 
-        y = y.unsqueeze(0)
-        denoised = self.model(y, (1.0 - t).unsqueeze(0), **self.extra_args)
-        return (y.squeeze(0) - denoised.squeeze(0)) / (t - 1.0)
+        denoised = self.model(y.unsqueeze(0), t.unsqueeze(0), **self.extra_args)
+        return (y - denoised.squeeze(0)) / t
 
-    def _callback(self, t0, y0):
+    def _callback(self, t0, y0, step):
         if self.callback is not None:
-            fake_step = round(t0.item() * self.n_steps)
             y0 = y0.unsqueeze(0)
-            sigma = 1.0 - t0
 
             self.callback({
                 "x": y0,
-                "i": fake_step,
-                "sigma": sigma,
-                "sigma_hat": sigma,
+                "i": step,
+                "sigma": t0,
+                "sigma_hat": t0,
                 "denoised": y0, # for a bad latent preview
             })
 
     def callback_step(self, t0, y0, dt):
-        if not self.is_adaptive:
-            self.pbar.update(1)
-            self._callback(t0, y0)
+        if self.is_adaptive:
+            return
+
+        self._callback(t0, y0, self.step)
+
+        self.pbar.update(1)
+        self.step += 1
 
     def callback_accept_step(self, t0, y0, dt):
-        if self.is_adaptive:
-            self.pbar.update(dt.item())
-            self._callback(t0, y0)
+        if not self.is_adaptive:
+            return
+
+        progress = (self.t_max - t0.item()) / (self.t_max - self.t_min)
+
+        self._callback(t0, y0, round((self.n_steps - 1) * progress))
+
+        new_step = round(100 * progress)
+        self.pbar.update(new_step - self.step)
+        self.step = new_step
+
+    def reset(self):
+        self.step = 0
+        self.pbar.reset()
 
 class ODESampler:
     def __init__(self, solver, rtol, atol, max_steps):
@@ -58,17 +88,23 @@ class ODESampler:
 
     @torch.no_grad()
     def __call__(self, model, x: torch.Tensor, sigmas: torch.Tensor, extra_args=None, callback=None, disable=None):
+        t_max = sigmas.max()
+        t_min = sigmas.min()
+        n_steps = len(sigmas)
+
         if self.solver in FIXED_SOLVERS:
-            t = 1.0 - sigmas
+            t = sigmas
             is_adaptive = False
         else:
-            t = torch.stack([1.0 - sigmas.max(), 1.0 - sigmas.min()])
+            t = torch.stack([t_max, t_min])
             is_adaptive = True
 
-        ode = ODEFunction(model, n_steps=len(sigmas), is_adaptive=is_adaptive, callback=callback, extra_args=extra_args)
+        ode = ODEFunction(model, t_min, t_max, n_steps, is_adaptive=is_adaptive, callback=callback, extra_args=extra_args)
 
         samples = torch.empty_like(x)
-        for i in trange(x.shape[0], disable=disable):
+        for i in trange(x.shape[0], desc=self.solver, disable=disable):
+            ode.reset()
+
             samples[i] = torchdiffeq.odeint(
                 ode,
                 x[i],
@@ -84,14 +120,12 @@ class ODESampler:
             )[-1]
 
         if callback is not None:
-            sigma = sigmas.min()
-
             callback({
                 "x": samples,
-                "i": len(sigmas) - 1,
-                "sigma": sigma,
-                "sigma_hat": sigma,
-                "denoised": samples, # only accurate if sigma = 0, for now
+                "i": n_steps - 1,
+                "sigma": t_min,
+                "sigma_hat": t_min,
+                "denoised": samples, # only accurate if t_min = 0, for now
             })
 
         return samples
@@ -104,7 +138,7 @@ class ODESamplerSelect:
                 "solver": (SOLVERS, { "default": "bosh3" }),
                 "log_relative_tolerance": ("FLOAT", { "min": -7, "max": 0, "default": -2.5, "step": 0.1 }),
                 "log_absolute_tolerance": ("FLOAT", { "min": -7, "max": 0, "default": -3.5, "step": 0.1 }),
-                "max_steps": ("INT", { "min": 1, "max": 150, "default": 30, "step": 1 }),
+                "max_steps": ("INT", { "min": 1, "max": 500, "default": 30, "step": 1 }),
             }
         }
 
